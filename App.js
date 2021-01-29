affe21e38edf2264ad8e82f987ec57af2a52d270
MariaKlimova <masha-klimova@inbox.ru>
2021-01-29 18:17:56 +0300

    expo project started

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { AddTodo } from './src/AddTodo';
+
+import {Navbar} from './src/Navbar'
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Navbar title="Todo App"/>
+      <AddTodo/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
